8f03a7416fed1c2eb19bb776812116e7824d98c9
sofiap <sofiap@sofiap-pc.dom.unitech.com.ar>
2019-04-28 19:27:13 -0300

    28/04/2019

--- TPAnual/src/edu/usal/negocio/dao/factory/ClienteFactory.java
+++ TPAnual/src/edu/usal/negocio/dao/factory/ClienteFactory.java
@@ -1,13 +1,14 @@
 package edu.usal.negocio.dao.factory;
 
-import edu.usal.negocio.dao.implementacion.ClienteDAOImplArchivo;
+
+import edu.usal.negocio.dao.implementacion.ClienteDAOImplSerializacion;
 import edu.usal.negocio.dao.interfaces.ClienteDAO;
 
 public class ClienteFactory {
 	
 	public static ClienteDAO getImplementation(String source) {
 		if (source.equals("Archivo")) 
-			return new ClienteDAOImplArchivo();
+			return new ClienteDAOImplSerializacion();
 		return null;
 	}
 
